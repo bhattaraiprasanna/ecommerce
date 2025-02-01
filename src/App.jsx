936113a4eaf22387df4a035acd547ffa97aef351
Prasanna Bhattarai <prasannabhattarai36@gmail.com>
2025-02-01 19:47:01 +0545

    setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
