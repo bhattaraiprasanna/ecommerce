fb4a21596c19b16c695c770d9b28d58148bef613
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-03 13:49:31 +0545

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,27 @@ import { FaSearch , FaShoppingCart , FaUser} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
-            <div>
+    <nav  className='bg-white shadow-md'>
+        <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
+            <div className='text-lg font-bold'>
                 <Link to="/"> e-SHOP </Link>
             </div>
+            <div className='relative flex-1 mx-4'>
             <form >
-                <input type="text" placeholder='Search Product' />
-                <FaSearch  />
+                <input type="text" placeholder='Search Product' 
+                className='w-full border py-2 px-4' />
+                <FaSearch className='absolute top-3 right-3 text-red-500'></FaSearch>
             </form>
+            </div>
         </div>
-        <div>
+        <div className='flex'>
             <Link to="/cart">
             <FaShoppingCart />
             </Link>
-            <button>
+            <button className='hidden md:block'>
                 Login | Register
             </button>
-            <button>
+            <button className='block md:hidden'>
                 <FaUser />
 
             </button>
