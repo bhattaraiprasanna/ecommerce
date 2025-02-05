f350e2f0916135a303112aee3bda239720491d10
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-05 17:46:36 +0545

    image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Categories } from '../assets/mockData'
+import HeroImage from '../assets/Images/hero-page.jpg'
 const Home = () => {
   return (
     <div>
@@ -16,8 +17,13 @@ const Home = () => {
             </ul>
           </div>
           <div>
-           
-            
+            <img src={HeroImage} alt="" />
+            <div>
+              <p>Best products available</p>
+              <h2>WELCOME TO E-SHOP</h2>
+              <p>MILLIONS+ PRODUCTS</p>
+              <button>SHOP NOW</button>
+            </div>
           </div>
         </div>
       </div>
