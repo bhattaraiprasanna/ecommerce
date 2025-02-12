b1fa0ca2413994af8a6319e16e54a0eb499e19d3
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-12 17:11:00 +0545

    new

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,3 +1,14 @@
+import Image1 from "../assets/Images/bag.jpg"
+import Image2 from "../assets/Images/sandle.jpg"
+import Image3 from "../assets/Images/watch.jpg"
+import Image4 from "../assets/Images/cap.jpg"
+import Image5 from "../assets/Images/boot.jpg"
+import Image6 from "../assets/Images/dove.jpg"
+import Image7 from "../assets/Images/headphone.jpg"
+import Image8 from "../assets/Images/robot.jpg"
+import Image9 from "../assets/Images/wwatch.jpg"
+import Image10 from "../assets/Images/shirt.jpg"
+
 export const Categories =[
     "Electronics",
     "Fashion",
@@ -6,3 +17,66 @@ export const Categories =[
     "Sports",
     "Automotive",
 ];
+
+export const mockData =[
+    {
+        id:1,
+        image: Image9,
+        name: "Product 1",
+        price: 29.99,
+    },
+    {
+        id:2,
+        image: Image8,
+        name: "Product 2",
+        price: 39.99,
+    },
+    {
+        id:3,
+        image: Image7,
+        name: "Product 3",
+        price: 29.99,
+    },
+    {
+        id:4,
+        image: Image6,
+        name: "Product 4",
+        price: 29.99,
+    },
+    {
+        id:5,
+        image: Image5,
+        name: "Product 5",
+        price: 29.99,
+    },
+    {
+        id:6,
+        image: Image4,
+        name: "Product 6",
+        price: 29.99,
+    },
+    {
+        id:7,
+        image: Image3,
+        name: "Product 7",
+        price: 29.99,
+    },
+    {
+        id:8,
+        image: Image2,
+        name: "Product 8",
+        price: 29.99,
+    },
+    {
+        id:9,
+        image: Image1,
+        name: "Product 9",
+        price: 29.99,
+    }, 
+    {
+        id:10,
+        image: Image0,
+        name: "Product 10",
+        price: 29.99,
+    },
+]
