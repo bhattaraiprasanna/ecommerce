c2fb7ba15c8b26a1151af86092ee31f7d3872a02
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-18 10:54:48 +0545

    add

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode> 
+    <Provider store={store}> 
       <App />
+      </Provider>
   </React.StrictMode>
 );
