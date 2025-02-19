85b17e09c414f0c221969965a6e74d6cba9727c7
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-19 17:17:53 +0545

    shop

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -3,19 +3,23 @@ import { FaStar } from 'react-icons/fa'
 
 function ProductCart ({product}) {
   return (
-    <div>
-      <img src={product.image} alt="" /> 
-      <h3>{product.name} </h3>
-      <p>${product.price} </p>
-      <div>
-      <FaStar></FaStar>
-      <FaStar></FaStar>
-      <FaStar></FaStar>
-      <FaStar></FaStar>
+    <div className='bg-white p-4 shadow  rounded relative border-none transform transition-transform duration-300 hover:scale-105'>
+      <img src={product.image} alt="product.name" 
+      className='w-full h-48 object-contain mb-4' />
+
+      <h3 className='text-lg font-semibold'>{product.name} </h3>
+      <p className='text-gray-500'>${product.price} </p>
+      <div className='flex item-center mt-2 '>
+      <FaStar className='text-yellow-500' />
+      <FaStar className='text-yellow-500' />
+      <FaStar className='text-yellow-500' />
+      <FaStar className='text-yellow-500' />
+      
       </div>
-      <div>
-        <span>+</span>
-        <span> Add to Cart</span>
+      <div class='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all'
+      >
+        <span class="group-hover:hidden">+</span>
+        <span class="hidden group-hover:block"> Add to Cart</span>
       </div>
     </div>
   )
