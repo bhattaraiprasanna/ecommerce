b87ae4533382e66c7bc5bc1cfe4c359db0c97248
bhattaraiprasanna <prasannabhattarai36@gmail.com>
2025-02-21 12:33:27 +0545

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,34 +14,36 @@ const Footer = () => {
           <h4 className='text-lg font-semibold'>Quick Links</h4>
           <ul>
           <li>
-              <Link>Home</Link>
+              <Link to="/" className='hover:underline'>Home</Link>
             </li>
             <li>
-              <Link>Shop</Link>
+              <Link to="/shop" className='hover:underline'>Shop</Link>
             </li>
             <li>
-              <Link>Contact</Link>
+              <Link to="/contact" className='hover:underline'>Contact</Link>
             </li>
             <li>
-              <Link>About</Link>
+              <Link to="/about" className='hover:underline'>About</Link>
             </li>
           </ul>
         </div>
 
-        <div> 
-          <h4>Follow us</h4>
-          <div>
-          <a href=''><FaFacebook /></a>
-          <a href=''><FaTwitter /></a>
-          <a href=''><FaGithub /></a>
-          <a href=''><FaLinkedin /></a>
+        <div > 
+          <h4 className='text-lg font-semibold'>Follow us</h4>
+          <div className='flex space-x-4 mt-4 '>
+          <a href='' className='hover:text-gray-400'><FaFacebook /></a>
+          <a href='' className='hover:text-gray-400'><FaTwitter /></a>
+          <a href='' className='hover:text-gray-400'><FaGithub /></a>
+          <a href='' className='hover:text-gray-400'><FaLinkedin /></a>
           </div>
-          <form>
-            <input type='email' />
-            <button>Subscribe</button>
+          <form className='flex items-center justify-center mt-8'>
+            <input type='email' placeholder='Enter Email'
+            className='w-full p-2 rounded-lg bg-gray-800 border rounded-l-lg border-gray-600' />
+            <button className='bg-red-600 text-white px-4 py-2 rounded-r-lg '>Subscribe</button>
           </form>
         </div>
       </div>
+      
     </footer>
   )
 }
